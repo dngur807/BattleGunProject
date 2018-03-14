01042f0ba43833d2bb5116276aa8a0b3279182ec
topojs8@naver.com <ohjunsuck>
2018-03-15 01:23:37 +0900

    프레임워크 통합

--- BattleGunProject/오준석/UnRealTournment1/Client/Headers/Define.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define WINCX 800
-#define WINCY 600
-
-#define S_CAST(ClassName, _data) static_cast<ClassName>(_data)
-#define D_CAST(ClassName, _ptr) dynamic_cast<ClassName*>(_ptr)
