212b2810f3d15017dc2be83532fe0c9ba6ad4217
ohjunsuck <topojs8@naver.com>
2018-03-25 04:41:12 +0900

    한파일로병합.

--- BattleGunProject/오준석/04 랜더러 구성 및 플레이어 출력/Engine/Export/Export_Resources.inl
+++
@@ -1,5 +0,0 @@
-
-inline CResourceMgr* Get_ResourceMgr(void)
-{
-	return CResourceMgr::GetInstance();
-}
--- BattleGunProject/오준석/05 FpsCamera DynamicCamera 추가/Client/Default/Struct.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Enum.h"
-
-typedef struct tagGameInfo//게임 매니저가 들고 있음
-{
-	eMapType		m_eMapType;
-}GAMEINFO;
